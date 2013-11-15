55bb3c2574c1345d7678078cde35d6d62a66499b
Alexey <lavarana8@gmail.com>
2013-11-15 01:02:28 -0800

    Update include.js

--- include.js
+++ include.js
@@ -1,4 +1,52 @@
 /**
+ *	Core functions.
+ *	@version: 0.1b
+ *	@Author: Alexey N Sindyashkin (lavarana8@gmail.com)
+ *	@License: free for all.
+*/
+;(function(){
+
+/**
+ *	Clear array function. 
+ *	If array have subarray or subobjects - then clear them.
+*/
+Array.prototype.clear = function() {
+	for(var i=0,l=this.length;i<l;i++) {
+		var r = this.pop();
+		if(typeof(r)=='object')
+			if(r.constructor.name=='Array')
+				r = r.clear();
+			else if('clear' in this) 
+				r = r.clear();
+			else 
+				r = null;
+		delete r;
+	}
+	return null;
+}
+
+
+/**
+ *	Method 'Array.have'.
+ *	Is element already in array. 
+ *	@param ArrayElement
+ *	@return True if element in array or False elswere
+*/
+Array.prototype.have = (Array.prototype.indexOf==undefined) ?
+	function (val) {return this.indexOf(val)!=-1}:
+	function (val) {var i=this.length;while(i--)if(this[i]===val){delete i;return true;}delete i;return false}
+
+
+/**
+ *	Method 'Array.indexOf'.
+ *	return index of element in array
+ *	@param ArrayElement
+ *	@return Array index
+*/
+if(Array.prototype.indexOf==undefined)Array.prototype.indexOf=function(val){var i=this.length;while(i--)if(this[i]===val){return i;}delete i;return false}
+})(Array);
+
+/**
  *	Create function include for javascript.
  *	Use: just include this js file and after document init type include('path/to/lib/name.js');
  *	in object 'window.jsIncArr' you can see all included or not included js files
